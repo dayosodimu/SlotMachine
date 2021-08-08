006f91b4fa8fa1eee15160491cf974eaf05f06f3
desoulchild <33025335+dayosodimu@users.noreply.github.com>
2021-08-08 17:57:47 -0400

    Add project files.

---
+++ SlotMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlotMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Slot Machine");
+        }
+    }
+}
